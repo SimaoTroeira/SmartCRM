647d0e0d51e007e03f5b7a900508c1ece0cf53ff
simaotroeira <2201701@my.ipleiria.pt>
2025-07-05 17:05:01 +0100

    frontend modal de desativar

--- frontend/src/components/companies/Company.vue
+++ frontend/src/components/companies/Company.vue
@@ -255,12 +255,10 @@
     </div>
   </div>
 
-
   <div v-if="showDeactivateModal"
     class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 transition-all duration-300 ease-in-out">
-    <div class="bg-white p-6 rounded-lg shadow-md w-96 transform transition-all duration-300"
-      :class="{ 'scale-100 opacity-100': showDeactivateModal, 'scale-95 opacity-0': !showDeactivateModal }">
-      <h3 class="text-lg font-bold mb-4">Tem certeza que deseja desativar esta empresa?</h3>
+    <div class="modal-wrapper w-96">
+      <h3 class="text-lg font-bold mb-4 text-center">Tem certeza que deseja desativar esta empresa?</h3>
       <div class="flex justify-end gap-2">
         <button type="button" @click="closeDeactivateModal" class="btn btn-secondary">Cancelar</button>
         <button type="button" @click="deactivateCompany" class="btn btn-danger">Desativar</button>
